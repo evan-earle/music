0cc37ef921e50424877e0b9a62cb4bf6d0f02ef9
Ozark19 <55114864+Ozark19@users.noreply.github.com>
2019-12-31 16:07:05 -0500

    Updates

---
+++ precache-manifest.026e88b535cae0a90816eb525e1d45f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2a4223ec2f016babbdb9946a326104",
+    "url": "/ozark19/music/index.html"
+  },
+  {
+    "revision": "f1ee36854ccdfdaf12f9",
+    "url": "/ozark19/music/static/css/main.3618b4dc.chunk.css"
+  },
+  {
+    "revision": "c6b2bd418652b18f36c0",
+    "url": "/ozark19/music/static/js/2.52e377f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ozark19/music/static/js/2.52e377f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1ee36854ccdfdaf12f9",
+    "url": "/ozark19/music/static/js/main.35e7233b.chunk.js"
+  },
+  {
+    "revision": "acdfbcbe259345685ccb",
+    "url": "/ozark19/music/static/js/runtime-main.aacb4baa.js"
+  },
+  {
+    "revision": "236a33fd2ce0757b7fbfd0f1ea92b9ea",
+    "url": "/ozark19/music/static/media/city.236a33fd.jpg"
+  },
+  {
+    "revision": "013e4a6e584626cb4ad0b6eb9094ec80",
+    "url": "/ozark19/music/static/media/plant.013e4a6e.jpg"
+  },
+  {
+    "revision": "3553bf5d27a68d0373c0562a2407d2db",
+    "url": "/ozark19/music/static/media/sky.3553bf5d.jpg"
+  }
+]);
